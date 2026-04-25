0a33ddf11f02785eb12dcdb9fc855bae2c60c461
raybex343 <ochusomtochukwu@gmail.com>
2026-04-25 04:20:10 -0300

    Add task list and task count elements to app.js

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 const taskInput = document.getElementById('taskInput');
 const submitBtn = document.getElementById('submitBtn');
+const taskList = document.getElementById('task-list');
+const taskCount = document.getElementById('taskCount');
+const clearAll = document.getElementById('clearAll');
